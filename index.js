727ebbc8b29651c76bc40296e71f16847f888005
Nat Weerawan <nat@cmmc.io>
2018-03-14 13:19:03 +0700

    refactor.

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-var fs = require('fs');
-var path = require('path');
-const HAP = require('hap-nodejs');
-const pkgHap = require('./node_modules/hap-nodejs/package.json');
-var accessoryLoader = require('./node_modules/hap-nodejs/lib/AccessoryLoader');
-const pkg = require('./package.json'); 
-const log = require('yalm'); 
+const path = require('path')
+const HAP = require('hap-nodejs')
+const {uuid, Bridge, Accessory} = HAP
+const pkgHap = require('./node_modules/hap-nodejs/package.json')
+const accessoryLoader = require('./node_modules/hap-nodejs/lib/AccessoryLoader')
+const pkg = require('./package.json')
+const log = require('yalm')
 
-log.setLevel('verbose'); 
-log(pkg.name + ' ' + pkg.version + ' starting'); 
-log.info('using hap-nodejs version', pkgHap.version);
+/*
+ * Config Section
+ */
 
-const {uuid, Bridge, Accessory, Service, Characteristic} = HAP;
 const bridgeName = 'Nat Siri Bridge'
 
+log.setLevel('verbose')
+log(pkg.name + ' ' + pkg.version + ' is starting')
+log.info(`using ${pkgHap.name} version ${pkgHap.version}`)
+
 HAP.init()
-var bridge = new Bridge(bridgeName, uuid.generate(bridgeName));
 
-// Load up all accessories in the /accessories folder
-var dir = path.join(__dirname, "accessories");
-var accessories = accessoryLoader.loadDirectory(dir);
-// Add them all to the bridge
-accessories.forEach(function(accessory) {
-    bridge.addBridgedAccessory(accessory);
-});
+const bridge = new Bridge(bridgeName, uuid.generate(bridgeName))
+const accessories = accessoryLoader.loadDirectory(path.join(__dirname, 'accessories'))
+accessories.forEach(accessory => bridge.addBridgedAccessory(accessory))
 
-// Listen for bridge identification event
-bridge.on('identify', function(paired, callback) {
-  console.log("Node Bridge identify");
-  callback(); // success
-});
+bridge.on('identify', (paired, callback) => {
+  console.log('Node Bridge identify')
+  callback() // success
+})
 
 // Publish the Bridge on the local network.
 bridge.publish({
-    username: "CC:22:3D:E3:CE:F6",
-    port: 51826,
-    pincode: "031-45-154",
-    category: Accessory.Categories.BRIDGE
-});
+  username: 'CC:22:3D:E3:CE:F6',
+  port: 51826,
+  pincode: '031-45-154',
+  category: Accessory.Categories.BRIDGE
+})
 
