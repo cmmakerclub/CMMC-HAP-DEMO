4889869b097892a70a0239b3d45565c9a2113b71
Nat <nat@cmmc.io>
2018-09-29 22:37:28 +0700

    update.

--- index.js
+++ index.js
@@ -1,41 +1,82 @@
-const path = require('path')
-const HAP = require('hap-nodejs')
-const {uuid, Bridge, Accessory} = HAP
-const pkgHap = require('./node_modules/hap-nodejs/package.json')
-const accessoryLoader = require('./node_modules/hap-nodejs/lib/AccessoryLoader')
-const pkg = require('./package.json')
-const log = require('yalm')
-const fetch = require('node-fetch')
+const path = require('path');
+const HAP = require('hap-nodejs');
+const {uuid, Bridge, Accessory} = HAP;
+const pkg = require('./package.json');
+const log = require('yalm');
+const fetch = require('node-fetch');
+const program = require('commander');
+const configStore = require('./Configstore');
+const fs = require('fs');
+
+const pkgHap = require(__dirname + '/node_modules/hap-nodejs/package.json');
+const accessoryLoader = require(__dirname + '/node_modules/hap-nodejs/lib/AccessoryLoader');
+
+let ACCESSORIES_PATH = configStore.get('ACCESSORIES_PATH');
+
+let username = 'C1:22:3D:E3:CE:F6';
+let pincode = '031-45-154';
 
 /*
  * Config Section
  */
 
-const bridgeName = 'Nat Siri Bridge'
-const accessoriesDir = path.join(__dirname, 'accessories')
+program.name(pkg.name).
+    usage('[options]').
+    version(pkg.version).
+    option('-i, --init', 'Initialize accessories ').
+    option('-b, --bridgeName', 'Bridge Name');
+program.parse(process.argv);
 
+if (program.init) {
+  log.info(`initializing`);
+  const accessoriesPath = `${process.cwd()}`;
+  configStore.set('ACCESSORIES_PATH', accessoriesPath);
+  ACCESSORIES_PATH = accessoriesPath;
+}
 
+if (!ACCESSORIES_PATH) {
+  log.error(`cmmc-hapjs --init must be called first.`);
+  process.exit(1);
+}
 
-log.setLevel('verbose')
-log(pkg.name + ' ' + pkg.version + ' is starting')
-log.info(`using ${pkgHap.name} version ${pkgHap.version}`)
+let bridgeName = 'Default Siri Bridge';
+if (program.bridgeName) {
+  bridgeName = program.args.join(' ');
+}
 
-HAP.init()
+if (!fs.existsSync(ACCESSORIES_PATH)) {
+  log.error(`${ACCESSORIES_PATH} not found.`);
+  process.exit(1);
+}
 
-const bridge = new Bridge(bridgeName, uuid.generate(bridgeName))
-const accessories = accessoryLoader.loadDirectory(accessoriesDir)
-accessories.forEach(accessory => bridge.addBridgedAccessory(accessory))
+console.log(bridgeName);
+const accessoriesDir = ACCESSORIES_PATH;
+
+log.setLevel('verbose');
+log(`${pkg.name} ${pkg.version} is starting.`);
+log(`bridgeName=${bridgeName}, pinCode=${pincode}, username=${username}`);
+log.info(`using ${pkgHap.name} version ${pkgHap.version}`);
+
+HAP.init(ACCESSORIES_PATH + '/persist');
+const bridge = new Bridge(bridgeName, uuid.generate(bridgeName));
+try {
+  const accessories = accessoryLoader.loadDirectory(accessoriesDir);
+  accessories.forEach(accessory => bridge.addBridgedAccessory(accessory));
+}
+catch (e) {
+  // console.log(e);
+}
 
 bridge.on('identify', (paired, callback) => {
-  console.log('Node Bridge identify')
-  callback() // success
-})
+  console.log(`[${bridgeName} Node Bridge identify`);
+  callback(); // success
+});
 
 // Publish the Bridge on the local network.
 bridge.publish({
-  username: 'CC:22:3D:E3:CE:F6',
+  username: username,
   port: 51826,
-  pincode: '031-45-154',
-  category: Accessory.Categories.BRIDGE
-})
+  pincode: pincode,
+  category: Accessory.Categories.BRIDGE,
+});
 
